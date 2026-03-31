5c0a9c94768e4ff962b6c8626f143224b5e077a1
Martin <odachi054@gmail.com>
2026-03-31 23:43:06 +0100

    logparse: improved error message

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,31 @@ use std::process;
 
 fn main() {
 
-    let args : Vec<String>  = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("An Error occured: invalid file path");
-        eprintln!("Usage: cargo run -- <file path>");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Error: No file path provided");
+        eprintln!("Usage: cargo run -- <file path>          - For total log entries\n       cargo run -- <file path> <keyword> - Search log for keyword" );
         process::exit(1);
+
     }
+
     let sys_log = fs::read_to_string(&args[1])
-    .expect("Failed to read System log file");
-    //println!("{sys_log}");
-    
-    let mut error_count = 0;
+    .expect("Failed to read System log");
+
+    // println!("{sys_log}");
+    let sys_log_count = sys_log.lines().count();
+
+    println!("Total system log: {sys_log_count} ");
+
+    let mut error_count  = 0;
+
     for line in sys_log.lines() {
         if line.contains(&args[2]) {
-            println!("{line}");
             error_count += 1;
 
         }
-        
     }
-    println!("Found {error_count} matches for '{}'", &args[2]);
-    let sys_log_count  = sys_log.lines().count();
-    println!("The number of system logs: {sys_log_count}");
+    println!("Found {error_count} matches for keyword {}", &args[2]);
+        
+    
 }
