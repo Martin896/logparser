f6d18be90886c93866142dd0656f5a940e700475
Martin <odachi054@gmail.com>
2026-04-08 06:27:25 +0100

    logparse: rewrite from memory

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,35 @@ use std::process;
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
+    let args : Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Error: No file path provided");
-        eprintln!("Usage: cargo run -- <file path>          - For total log entries\n       cargo run -- <file path> <keyword> - Search log for keyword" );
+        eprintln!("Expected Error: No file path provided");
+        eprintln!("Usage: cargo run -- <file path>");
         process::exit(1);
 
-    } 
+    }
    
 
     let sys_log = fs::read_to_string(&args[1])
-    .expect("Failed to read System log");
+    .expect("Failed to read file system");
 
-    // println!("{sys_log}");
-    let sys_log_count = sys_log.lines().count();
+    let count_log = sys_log.lines().count();
+    println!("Total system log: {count_log}");
 
-    println!("Total system log: {sys_log_count} ");
-
-    let mut error_count  = 0;
+    let mut keyword_count = 0;
+    
     if args.len() >= 3 {
-        for line in sys_log.lines() {
-            if line.contains(&args[2]) {
-                error_count += 1;
+        for line in sys_log.lines(){
 
-            }
-       
-       
+            if line.contains(&args[2]){
+              
+            keyword_count +=1;
+
         }
-        println!("Found {error_count} matches for keyword {}", &args[2]); 
-    }
-    
         
+    }
+    println!("Found {keyword_count} matches for the keyword {}", &args[2]);
     
+    }
+
 }
